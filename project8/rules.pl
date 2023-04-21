5c0a97359c414ceb29e65a1cac04d49870bf89d2
themnsavage <mnsavage@crimson.ua.edu>
2023-04-21 15:25:42 -0500

    fix(rules): fix hourly and commision pay

--- project8/rules.pl
+++ project8/rules.pl
@@ -3,24 +3,13 @@ pay(First_name, Last_name, Pay):- salaried(First_name, Last_name, Salary),
 
 pay(First_name, Last_name, Pay):- hourly(First_name, Last_name, Hours, Rate),
     (
-    Hours =< 40,
-    Pay is (Hours*Rate);
-    
-    Hours =< 50,
-    Over_time_hours is (Hours - 40),
-    Over_time_rate is (1.5 * Rate),
-    Pay is ((40 * Rate) + (Over_time_rate * Over_time_hours));
-
-    Hours > 50,
-    First_over_time_hours is (10),
-    First_over_time_rate is (1.5 * Hours),
-    Second_over_time_hours is (Hours - 50),
-    Second_over_time_rate is (2 * Rate),
-    Pay is ((Rate * 40) + (First_over_time_hours * First_over_time_rate) + (Second_over_time_hours * Second_over_time_rate))
+    Hours =< 40, Pay is (Hours * Rate);
+    Hours =< 50, Hours > 40, Pay is ((40 * Rate) + ((Hours - 40) * (1.5 * Rate)));
+    Hours > 50, Pay is ((Rate * 40) + (10 * (1.5 * Rate)) + ((Hours - 50) * (2 * Rate)))
     ).
+
 pay(First_name, Last_name, Pay):- commission(First_name, Last_name, Min_salary, Amount, Rate),
     (
-    (Amount * Rate) > Min_salary,
-    Pay is (Amount*Rate);
-    Pay is (Min_salary)
+    (Amount * Rate) < Min_salary, Pay is (Min_salary);
+    (Amount * Rate) >= Min_salary, Pay is (Amount * Rate)
     ).
