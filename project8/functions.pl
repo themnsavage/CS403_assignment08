1cd8cad2376a0d6cfd95fdd2ac865cf2fe226fac
themnsavage <mnsavage@crimson.ua.edu>
2023-04-21 12:14:35 -0500

    feat(min_functions): min_function implemented

--- project8/functions.pl
+++ project8/functions.pl
@@ -17,12 +17,12 @@ list(Op, Ref, List) :- (
     ).
 
 min(Op, Ref, Min) :- (
-    Op=('=='), findall(Z, (pay(X, Y, Z), Z=:=Ref), List),  length(List, Count);
-    Op=('!='), findall(Z, (pay(X, Y, Z), Z=\=Ref), List),  length(List, Count);
-    Op=('>'), findall(Z, (pay(X, Y, Z), Z>Ref), List),  length(List, Count);
-    Op=('>='), findall(Z, (pay(X, Y, Z), Z>=Ref), List),  length(List, Count);
-    Op=('<'), findall(Z, (pay(X, Y, Z), Z<Ref), List),  length(List, Count);
-    Op=('<='), findall(Z, (pay(X, Y, Z), Z=<Ref), List),  length(List, Count)
+    Op=('=='), findall(Z, (pay(X, Y, Z), Z=:=Ref), List),list_min(List, M), Min is (M);
+    Op=('!='), findall(Z, (pay(X, Y, Z), Z=\=Ref), List),  length(List, Count),list_min(List, M), Min is (M);
+    Op=('>'), findall(Z, (pay(X, Y, Z), Z>Ref), List),  length(List, Count),list_min(List, M), Min is (M);
+    Op=('>='), findall(Z, (pay(X, Y, Z), Z>=Ref), List),  length(List, Count),list_min(List, M), Min is (M);
+    Op=('<'), findall(Z, (pay(X, Y, Z), Z<Ref), List),  length(List, Count),list_min(List, M), Min is (M);
+    Op=('<='), findall(Z, (pay(X, Y, Z), Z=<Ref), List),  length(List, Count),list_min(List, M), Min is (M)
     ).
 
 max(Op, Ref, Max) :- (
@@ -51,3 +51,11 @@ avg(Op, Ref, Avg) :- (
     Op=('<'), findall(Z, (pay(X, Y, Z), Z<Ref), List),  length(List, Count);
     Op=('<='), findall(Z, (pay(X, Y, Z), Z=<Ref), List),  length(List, Count)
     ).
+
+list_min([L|Ls], Min) :-
+    list_min(Ls, L, Min).
+
+list_min([], Min, Min).
+list_min([L|Ls], Min0, Min) :-
+    Min1 is min(L, Min0),
+    list_min(Ls, Min1, Min).
