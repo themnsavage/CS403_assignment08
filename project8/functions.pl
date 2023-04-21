eb07b23e00eefcfa14a7be0b524a93aea5cb8443
themnsavage <mnsavage@crimson.ua.edu>
2023-04-21 12:43:48 -0500

    feat(max_function): implemented max_function

--- project8/functions.pl
+++ project8/functions.pl
@@ -18,20 +18,20 @@ list(Op, Ref, List) :- (
 
 min(Op, Ref, Min) :- (
     Op=('=='), findall(Z, (pay(X, Y, Z), Z=:=Ref), List),list_min(List, M), Min is (M);
-    Op=('!='), findall(Z, (pay(X, Y, Z), Z=\=Ref), List),  length(List, Count),list_min(List, M), Min is (M);
-    Op=('>'), findall(Z, (pay(X, Y, Z), Z>Ref), List),  length(List, Count),list_min(List, M), Min is (M);
-    Op=('>='), findall(Z, (pay(X, Y, Z), Z>=Ref), List),  length(List, Count),list_min(List, M), Min is (M);
-    Op=('<'), findall(Z, (pay(X, Y, Z), Z<Ref), List),  length(List, Count),list_min(List, M), Min is (M);
-    Op=('<='), findall(Z, (pay(X, Y, Z), Z=<Ref), List),  length(List, Count),list_min(List, M), Min is (M)
+    Op=('!='), findall(Z, (pay(X, Y, Z), Z=\=Ref), List),list_min(List, M), Min is (M);
+    Op=('>'), findall(Z, (pay(X, Y, Z), Z>Ref), List), list_min(List, M), Min is (M);
+    Op=('>='), findall(Z, (pay(X, Y, Z), Z>=Ref), List), list_min(List, M), Min is (M);
+    Op=('<'), findall(Z, (pay(X, Y, Z), Z<Ref), List), list_min(List, M), Min is (M);
+    Op=('<='), findall(Z, (pay(X, Y, Z), Z=<Ref), List), list_min(List, M), Min is (M)
     ).
 
 max(Op, Ref, Max) :- (
-    Op=('=='), findall(Z, (pay(X, Y, Z), Z=:=Ref), List),  length(List, Count);
-    Op=('!='), findall(Z, (pay(X, Y, Z), Z=\=Ref), List),  length(List, Count);
-    Op=('>'), findall(Z, (pay(X, Y, Z), Z>Ref), List),  length(List, Count);
-    Op=('>='), findall(Z, (pay(X, Y, Z), Z>=Ref), List),  length(List, Count);
-    Op=('<'), findall(Z, (pay(X, Y, Z), Z<Ref), List),  length(List, Count);
-    Op=('<='), findall(Z, (pay(X, Y, Z), Z=<Ref), List),  length(List, Count)
+    Op=('=='), findall(Z, (pay(X, Y, Z), Z=:=Ref), List),list_max(List, M), Max is (M);
+    Op=('!='), findall(Z, (pay(X, Y, Z), Z=\=Ref), List),list_max(List, M), Max is (M);
+    Op=('>'), findall(Z, (pay(X, Y, Z), Z>Ref), List),list_max(List, M), Max is (M);
+    Op=('>='), findall(Z, (pay(X, Y, Z), Z>=Ref), List),list_max(List, M), Max is (M);
+    Op=('<'), findall(Z, (pay(X, Y, Z), Z<Ref), List),list_max(List, M), Max is (M);
+    Op=('<='), findall(Z, (pay(X, Y, Z), Z=<Ref), List),list_max(List, M), Max is (M)
     ).
 
 total(Op, Ref, Total) :- (
@@ -59,3 +59,8 @@ list_min([], Min, Min).
 list_min([L|Ls], Min0, Min) :-
     Min1 is min(L, Min0),
     list_min(Ls, Min1, Min).
+
+list_max([], R, R).
+list_max([X|Xs], WK, R):- X >  WK, list_max(Xs, X, R). 
+list_max([X|Xs], WK, R):- X =< WK, list_max(Xs, WK, R).
+list_max([X|Xs], R):- list_max(Xs, X, R).
